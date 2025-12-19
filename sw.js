378bc390bff704076e8065e92a826da7e835b7e8
marconeri70 <marconeri70@gmail.com>
2025-12-19 21:38:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,40 @@
-const CACHE = "cefalea-pwa-v7";
+// Diario Cefalea - sw.js (COMPLETO)
+const CACHE = "cefalea-pwa-v20"; // <-- cambia numero ad ogni update!
+
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  const req = event.request;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(() => cached);
+    })
   );
 });
